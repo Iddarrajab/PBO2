308a2ea4e75495229e2c03460111ecb4bfb4e29e
Iddarrajab <127711160+Iddarrajab@users.noreply.github.com>
2023-04-02 12:09:06 +0700

    Add files via upload

---
+++ LATIHAN 2/Latihan-2/Single-contoh 1.py
@@ -0,0 +1,30 @@
+# Nama   : muhammad iddar rajab
+# NIM    : 210511028
+# Kelas  : R1
+# Matkul : Pemrogaman Berorientasi Objek 2
+
+
+class Hewan:
+
+    def __init__(self, nama, umur):
+        self.nama = nama
+        self.umur = umur
+
+    def bergerak(self):
+
+        print(self.nama, "bergerak")
+
+
+class Kucing(Hewan):
+
+    def __init__(self, nama, umur, jenis_bulu):
+        super().__init__(nama, umur)
+        self.jenis_bulu = jenis_bulu
+
+    def bersuara(self):
+        print("Meow!")
+
+
+kucingA = Kucing("Kitty", 2, "Persia")
+kucingA.bergerak()  # output: Kitty bergerak
+kucingA.bersuara()  # output: Meow!
